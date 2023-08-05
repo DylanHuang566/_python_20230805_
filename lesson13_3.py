c10d15d03d16ee32d4c04deb95bbee3becc59df9
DylanHuang566 <135844898+DylanHuang566@users.noreply.github.com>
2023-08-05 04:00:07 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -1,14 +1,58 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# t = np.arange(0.,1.0,0.05)
+# print(t)
+# y1 = np.sin(2 * np.pi * t)
+# y2 = np.cos(2 * np.pi * t)
+# print(y1)
+# print(y2)
+# figure1 = plt.figure(figsize=(8,4))
+# axes1 = figure1.add_subplot()
+# axes1.plot(y1)
+# axes1.plot(y2)
+# plt.show()
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import streamlit as st
+
+# t = np.arange(0.,1.0,0.05)
+# st.write(t)
+# y1 = np.sin(2 * np.pi * t)
+# y2 = np.cos(2 * np.pi * t)
+# st.write(y1)
+# st.write(y2)
+# figure1 = plt.figure(figsize=(8,4))
+# axes1 = figure1.add_subplot()
+# axes1.plot(y1)
+# axes1.plot(y2)
+# st.write(figure1)
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import streamlit as st
+
+# st.slider("三角函式",min_value=0,max_value=10)
+# t = np.arange(0.,1.0,0.05)
+# y1 = np.sin(2 * np.pi * t)
+# y2 = np.cos(2 * np.pi * t)
+# figure1 = plt.figure(figsize=(8,4))
+# axes1 = figure1.add_subplot()
+# axes1.plot(y1)
+# axes1.plot(y2)
+# st.write(figure1)
+
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit as st
 
-t = np.arange(0.,1.0,0.05)
-print(t)
+value = st.slider("三角函式",min_value=0,max_value=10)
+t = np.arange(0.,value,0.05)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-print(y1)
-print(y2)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
 axes1.plot(y2)
-plt.show()
+st.write(figure1)
